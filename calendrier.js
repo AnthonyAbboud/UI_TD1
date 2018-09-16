40a1cd948c45a4eff9b2c211c70a6496f7d79871
Anthony <anthony.abboud@polymtl.ca>
2018-09-15 20:07:37 -0400

    Progression

--- calendrier.js
+++ calendrier.js
@@ -1,8 +1,34 @@
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
 window.onload = function() {
-	fetch('http://www.groupes.polymtl.ca/log2420/Lab/Doodle/cal-data.json')
+	fetch('https://raw.githubusercontent.com/AnthonyAbboud/UI_TD1/master/cal-data.json')
 	.then(function(response){
-		return response.json(); 
-	}).then(function(myJson){
-		document.getElementById("dates").innerHTML = myJson.Calendrier[1][1];
-	})};
+		return response.json();
+	})
+	.then(function(myJson){
+		var prefixDates = 'dates';
+		var arrayDates = new Array();
+		for(var i = 1; i <= myJson.Calendrier.length; i++){
+			arrayDates.push(new Date(myJson.Calendrier[i-1][0]));
+			document.getElementById(prefixDates + i).innerHTML = 
+				months[arrayDates[i-1].getMonth()] + "\n" + 
+				arrayDates[i-1].getDate() + "\n" + 
+				days[arrayDates[i-1].getDay()] + "\n\n" +
+				arrayDates[i-1].getHours() + ":" +
+				arrayDates[i-1].getMinutes() + "0\n" +
+				(arrayDates[i-1].getHours() + (myJson.Calendrier[i-1][1]/60)) + ":00";
+		}
+
+		for(var i = 1; i < 10; i++){
+			//document.getElementById(prefixDates + i).innerHTML = myJson.Calendrier[i-1][0];
+		}
+
+		document.getElementById("participNumber").innerHTML = myJson.Participants.length + " particpants";
+
+		var prefixNoms = 'Nom';
+		for(var i = 1; i < 7; i++){
+			document.getElementById(prefixNoms + i).innerHTML += myJson.Participants[i-1].Nom;
+		}
+	})
+};
