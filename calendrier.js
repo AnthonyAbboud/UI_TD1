5512458c221b23c3b0aa1778d450a5e4d94b7715
Anthony <anthony.abboud@polymtl.ca>
2018-10-03 11:30:10 -0400

    Push everything merged

--- calendrier.js
+++ calendrier.js
@@ -1,3 +1,7 @@
+/* Data Structures */
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
 var img=document.getElementsByClassName("thirdLine1");
 
 function mOver(obj) {
@@ -10,13 +14,13 @@ function mOut(obj) {
 
 
 
-    function clear_value (oInput)
-    {
-        if (!('value' in oInput))
-            oInput.placeholder = oInput.value;
-        if (oInput.value != oInput.placeholder)
-            oInput.value = '';
-    }
+function clear_value (oInput)
+{
+    if (!('value' in oInput))
+        oInput.placeholder = oInput.value;
+    if (oInput.value != oInput.placeholder)
+        oInput.value = '';
+}
 
     // var list = document.getElementById("firstLine")
     // fetch("calendrier.json")
@@ -28,4 +32,63 @@ function mOut(obj) {
     //     list.appendChild(listItem);
     //   }
     // });
-    
+
+/* Afficher Popup */
+var e = document.getElementById("parent");
+
+e.onmouseover = function() {
+  document.getElementById('popup').style.display = 'block';
+}
+
+e.onmouseout = function() {
+  document.getElementById('popup').style.display = 'none';
+}
+
+/* Fonction pour que les popup soient toujours au premier plan */
+
+function focusFunction() {
+    if (navigator.userAgent.indexOf('MSIE') > -1) {
+        self.focus();
+    } else {
+        self.close();
+    }
+}
+
+/* Changer l'image */
+
+let img2 = document.getElementById('imgClickAndChange');
+img2.addEventListener('click', changeImage());
+
+function changeImage()  {
+   if(img2.classList.contains('checked')){
+     document.getElementById("imgClickAndChange").src = "tick-check.png";
+   }
+   else{
+     document.getElementById("imgClickAndChange").src = "check.png";
+   }
+   img2.classList.toggle('checked')
+}
+
+
+fetch('https://raw.githubusercontent.com/AnthonyAbboud/UI_TD1/master/cal-data.json')
+.then(function(response){
+    return response.json();
+})
+.then(function(myJson){
+    var prefixDates = 'date';
+    var arrayDates = new Array();
+    for(var i = 1; i <= myJson.Calendrier.length; i++){
+        arrayDates.push(new Date(myJson.Calendrier[i-1][0]));
+        document.getElementById(prefixDates + i).innerHTML = 
+            months[arrayDates[i-1].getMonth()] + "\n" + 
+            arrayDates[i-1].getDate() + "\n" + 
+            days[arrayDates[i-1].getDay()] + "\n\n" +
+            arrayDates[i-1].getHours() + ":" +
+            arrayDates[i-1].getMinutes() + "0\n" +
+            (arrayDates[i-1].getHours() + (myJson.Calendrier[i-1][1]/60)) + ":00";
+        document.getElementById("popup").innerHTML=document.getElementById(prefixDates + i).innerHTML;
+
+    }
+
+
+})
