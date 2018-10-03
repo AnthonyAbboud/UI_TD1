8bf7ad76f7903f547124ecbd27045c801425f6e8
nana <rochanafardon@hotmail.fr>
2018-10-03 10:32:12 -0400

    ajout des autres fichiers

--- calendrier.js
+++ calendrier.js
@@ -1,34 +1,31 @@
-var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-var days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+var img=document.getElementsByClassName("thirdLine1");
 
-window.onload = function() {
-	fetch('https://raw.githubusercontent.com/AnthonyAbboud/UI_TD1/master/cal-data.json')
-	.then(function(response){
-		return response.json();
-	})
-	.then(function(myJson){
-		var prefixDates = 'dates';
-		var arrayDates = new Array();
-		for(var i = 1; i <= myJson.Calendrier.length; i++){
-			arrayDates.push(new Date(myJson.Calendrier[i-1][0]));
-			document.getElementById(prefixDates + i).innerHTML = 
-				months[arrayDates[i-1].getMonth()] + "\n" + 
-				arrayDates[i-1].getDate() + "\n" + 
-				days[arrayDates[i-1].getDay()] + "\n\n" +
-				arrayDates[i-1].getHours() + ":" +
-				arrayDates[i-1].getMinutes() + "0\n" +
-				(arrayDates[i-1].getHours() + (myJson.Calendrier[i-1][1]/60)) + ":00";
-		}
+function mOver(obj) {
+  obj.innerHTML = img
+}
 
-		for(var i = 1; i < 10; i++){
-			//document.getElementById(prefixDates + i).innerHTML = myJson.Calendrier[i-1][0];
-		}
+function mOut(obj) {
+  obj.innerHTML = "blank"
+}
 
-		document.getElementById("participNumber").innerHTML = myJson.Participants.length + " particpants";
 
-		var prefixNoms = 'Nom';
-		for(var i = 1; i < 7; i++){
-			document.getElementById(prefixNoms + i).innerHTML += myJson.Participants[i-1].Nom;
-		}
-	})
-};
+
+    function clear_value (oInput)
+    {
+        if (!('value' in oInput))
+            oInput.placeholder = oInput.value;
+        if (oInput.value != oInput.placeholder)
+            oInput.value = '';
+    }
+
+    // var list = document.getElementById("firstLine")
+    // fetch("calendrier.json")
+    // .then(function(response) { return response.json(); })
+    // .then(function(json) {
+    // for(var i = 0; i < json.Calendrier.length; i++) {
+    //     var listItem = document.createElement('li');
+    //     listItem.innerHTML = json.Calendrier[i] ;
+    //     list.appendChild(listItem);
+    //   }
+    // });
+    
